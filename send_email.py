3a1aecb02116711d832da9b497d83b993a0310c6
yashkim24 <yashkimtani77@gmail.com>
2020-04-15 19:47:30 +0530

    Python script for sending email

---
+++ send_email.py
@@ -0,0 +1,44 @@
+import smtplib, ssl
+import argparse
+
+def sendEmail(sender,receiver,pd):
+    """
+        Method to send email to the particular user
+        parameter : sender_email, receiver_email, sender_password
+    """
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = sender  # Enter your address
+    receiver_email = receiver  # Enter receiver address
+    password = pd
+    message = """\
+    Subject: Hi there
+
+    This message is sent from Python."""
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+if __name__ == '__main__':
+	"""
+	Get the sender email,receiver email and sender gmail password from the command line
+	"""
+	try:
+		#Get the username and number of tweets from command line
+		parser = argparse.ArgumentParser(description='Pass the sender email, receiver email and sender password')
+		parser.add_argument('--sender_email')
+		parser.add_argument('--receiver_email')
+		parser.add_argument('--sender_password')
+
+		args = parser.parse_args()
+		sender_email = args.sender_email
+		receiver_email = args.receiver_email
+		sender_password = args.sender_password
+
+		sendEmail(sender_email,receiver_email,sender_password)
+
+	except Exception as ex:
+		print("An error occurred: " + str(ex))
